f9458dd3c8eddcf63d179d77b09ce3c77b95fdb2
Enigma2014 <arungudadappa@gmail.com>
2024-10-14 20:48:48 +0530

    Added multiplication function

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ def main_function():
     print('Hello World..!')
 
 ut.add_function(2,3)
+ut.mul_function(5,3)
--- utils.py
+++ utils.py
@@ -1,2 +1,5 @@
 def add_function(inp_1,inp_2):
     print(inp_1 + inp_2)
+
+def mult_function(inp_1,inp_2):
+    print(inp_1 * inp_2)
